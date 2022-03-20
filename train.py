796264d82c427d05347f211d9b247d35f6078d24
YaraSco <53176263+YaraSco@users.noreply.github.com>
2022-03-19 21:07:51 -0400

    Update train.py

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def main():
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
